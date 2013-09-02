2232ee19bdefede245ee7ee9f3743d4546f335b9
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-09-02 21:21:26 +0200

    Added support for GameStop wired controller
    Thanks to robertadamsnet

--- XBOXUSB.h
+++ XBOXUSB.h
@@ -42,11 +42,13 @@
 #define XBOX_VID                                0x045E  // Microsoft Corporation
 #define MADCATZ_VID                             0x1BAD  // For unofficial Mad Catz controllers
 #define JOYTECH_VID                             0x162E  // For unofficial Joytech controllers
+#define GAMESTOP_VID                            0x0E6F  // Gamestop controller
 
 #define XBOX_WIRED_PID                          0x028E  // Microsoft 360 Wired controller
 #define XBOX_WIRELESS_PID                       0x028F  // Wireless controller only support charging
 #define XBOX_WIRELESS_RECEIVER_PID              0x0719  // Microsoft Wireless Gaming Receiver
 #define XBOX_WIRELESS_RECEIVER_THIRD_PARTY_PID  0x0291  // Third party Wireless Gaming Receiver
+#define GAMESTOP_WIRED_PID                      0x0401  // Gamestop wired controller
 
 #define XBOX_REPORT_BUFFER_SIZE 14 // Size of the input report buffer
 
@@ -108,7 +110,7 @@ public:
          * @return     Returns true if the device's VID and PID matches this driver.
          */
         virtual boolean VIDPIDOK(uint16_t vid, uint16_t pid) {
-                return ((vid == XBOX_VID || vid == MADCATZ_VID || vid == JOYTECH_VID) && pid == XBOX_WIRED_PID);
+                return ((vid == XBOX_VID || vid == MADCATZ_VID || vid == JOYTECH_VID || vid == GAMESTOP_VID) && (pid == XBOX_WIRED_PID || pid == GAMESTOP_WIRED_PID));
         };
         /**@}*/
 
