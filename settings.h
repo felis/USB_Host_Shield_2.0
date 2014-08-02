b46ead88e8117ffc88228ebdaf88be129b558a62
Kristian Lauszus <lauszus@gmail.com>
2014-08-02 15:16:29 -0700

    Don't use Arduino SPI library if spi4teensy3 should be used

--- settings.h
+++ settings.h
@@ -137,7 +137,7 @@ e-mail   :  support@circuitsathome.com
 #define USING_SPI4TEENSY3 0
 #endif
 
-#if (defined(ARDUINO_SAM_DUE) && defined(__SAM3X8E__)) || ARDUINO >= 158
+#if ((defined(ARDUINO_SAM_DUE) && defined(__SAM3X8E__)) || ARDUINO >= 158) && !USING_SPI4TEENSY3
 #include <SPI.h> // Use the Arduino SPI library for the Arduino Due or if the SPI library with transaction is available
 #endif
 
