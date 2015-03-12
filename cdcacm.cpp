ff3a1d6797997c9a62b822e0dfce27707916d08c
Andrew J. Kroll <xxxajk@gmail.com>
2015-03-12 17:27:04 -0400

    fix debug printing

--- cdcacm.cpp
+++ cdcacm.cpp
@@ -222,9 +222,9 @@ Fail:
 }
 
 void ACM::EndpointXtract(uint8_t conf, uint8_t iface, uint8_t alt, uint8_t proto, const USB_ENDPOINT_DESCRIPTOR *pep) {
-        ErrorMessage<uint8_t > (PSTR("Conf.Val"), conf);
-        ErrorMessage<uint8_t > (PSTR("Iface Num"), iface);
-        ErrorMessage<uint8_t > (PSTR("Alt.Set"), alt);
+        //ErrorMessage<uint8_t > (PSTR("Conf.Val"), conf);
+        //ErrorMessage<uint8_t > (PSTR("Iface Num"), iface);
+        //ErrorMessage<uint8_t > (PSTR("Alt.Set"), alt);
 
         bConfNum = conf;
 
@@ -337,16 +337,16 @@ uint8_t ACM::SendBreak(uint16_t duration) {
 void ACM::PrintEndpointDescriptor(const USB_ENDPOINT_DESCRIPTOR* ep_ptr) {
         Notify(PSTR("Endpoint descriptor:"), 0x80);
         Notify(PSTR("\r\nLength:\t\t"), 0x80);
-        PrintHex<uint8_t > (ep_ptr->bLength, 0x80);
+        D_PrintHex<uint8_t > (ep_ptr->bLength, 0x80);
         Notify(PSTR("\r\nType:\t\t"), 0x80);
-        PrintHex<uint8_t > (ep_ptr->bDescriptorType, 0x80);
+        D_PrintHex<uint8_t > (ep_ptr->bDescriptorType, 0x80);
         Notify(PSTR("\r\nAddress:\t"), 0x80);
-        PrintHex<uint8_t > (ep_ptr->bEndpointAddress, 0x80);
+        D_PrintHex<uint8_t > (ep_ptr->bEndpointAddress, 0x80);
         Notify(PSTR("\r\nAttributes:\t"), 0x80);
-        PrintHex<uint8_t > (ep_ptr->bmAttributes, 0x80);
+        D_PrintHex<uint8_t > (ep_ptr->bmAttributes, 0x80);
         Notify(PSTR("\r\nMaxPktSize:\t"), 0x80);
-        PrintHex<uint16_t > (ep_ptr->wMaxPacketSize, 0x80);
+        D_PrintHex<uint16_t > (ep_ptr->wMaxPacketSize, 0x80);
         Notify(PSTR("\r\nPoll Intrv:\t"), 0x80);
-        PrintHex<uint8_t > (ep_ptr->bInterval, 0x80);
+        D_PrintHex<uint8_t > (ep_ptr->bInterval, 0x80);
         Notify(PSTR("\r\n"), 0x80);
 }
