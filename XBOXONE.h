05c71da9c1c7f20f874246cf147d2551cd91a9d3
Kristian Sloth Lauszus <lauszus@gmail.com>
2015-04-14 02:02:05 +0200

    Replace boolean with bool
    Please see: https://github.com/felis/USB_Host_Shield_2.0/commit/bd8c8e740419095f3c1d8875d199e16858058899

--- XBOXONE.h
+++ XBOXONE.h
@@ -93,7 +93,7 @@ public:
          * @param  pid The device's PID.
          * @return     Returns true if the device's VID and PID matches this driver.
          */
-        virtual boolean VIDPIDOK(uint16_t vid, uint16_t pid) {
+        virtual bool VIDPIDOK(uint16_t vid, uint16_t pid) {
                 return (vid == XBOX_VID && pid == XBOX_ONE_PID);
         };
         /**@}*/
