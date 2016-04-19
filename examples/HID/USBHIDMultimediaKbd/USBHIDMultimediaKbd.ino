854fb80e0891a8c82d0c84c8af84a3c93166e70b
Kristian Sloth Lauszus <lauszus@gmail.com>
2016-04-19 11:34:04 +0200

    Autoformat USBHIDMultimediaKbd example
    No code change

--- examples/HID/USBHIDMultimediaKbd/USBHIDMultimediaKbd.ino
+++ examples/HID/USBHIDMultimediaKbd/USBHIDMultimediaKbd.ino
@@ -11,17 +11,17 @@
 class HIDSelector : public HIDComposite
 {
 public:
-  HIDSelector(USB *p) : HIDComposite(p) {};
+    HIDSelector(USB *p) : HIDComposite(p) {};
 
 protected:
-  void ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id, uint8_t len, uint8_t *buf); // Called by the HIDComposite library
-  bool SelectInterface(uint8_t iface, uint8_t proto);
+    void ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id, uint8_t len, uint8_t *buf); // Called by the HIDComposite library
+    bool SelectInterface(uint8_t iface, uint8_t proto);
 };
 
 // Return true for the interface we want to hook into
 bool HIDSelector::SelectInterface(uint8_t iface, uint8_t proto)
 {
-  if(proto != 0)
+  if (proto != 0)
     return true;
 
   return false;
@@ -30,21 +30,19 @@ bool HIDSelector::SelectInterface(uint8_t iface, uint8_t proto)
 // Will be called for all HID data received from the USB interface
 void HIDSelector::ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id, uint8_t len, uint8_t *buf) {
 #if 1
-        if (len && buf)  {
-                Notify(PSTR("\r\n"), 0x80);
-                for (uint8_t i = 0; i < len; i++) {
-                        D_PrintHex<uint8_t > (buf[i], 0x80);
-                        Notify(PSTR(" "), 0x80);
-                }
-        }
+  if (len && buf)  {
+    Notify(PSTR("\r\n"), 0x80);
+    for (uint8_t i = 0; i < len; i++) {
+      D_PrintHex<uint8_t > (buf[i], 0x80);
+      Notify(PSTR(" "), 0x80);
+    }
+  }
 #endif
 }
-
 
 USB     Usb;
 //USBHub     Hub(&Usb);
 HIDSelector    hidSelector(&Usb);
-
 
 void setup()
 {
