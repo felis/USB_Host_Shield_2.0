fa836349012987c0802a2d32fc493b87e4ce1e0c
ardi69 <armin@diedering.de>
2018-10-11 23:40:36 +0200

    add forgotten function

--- usbhub.h
+++ usbhub.h
@@ -201,7 +201,11 @@ public:
                 return bAddress;
         };
 
-        virtual bool DEVCLASSOK(uint8_t klass) {
+		virtual uint8_t GetPortAddress() {
+			return bPortAddress;
+		};
+
+		virtual bool DEVCLASSOK(uint8_t klass) {
                 return (klass == 0x09);
         }
 
