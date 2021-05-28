e8358763b537fee42d58a507f9649a8527d78222
Anthony Rios <ar664@njit.edu>
2021-05-27 21:34:49 -0400

    Added support for Adafruit NRF52832 Feather
    Used this pr as a reference:
    https://github.com/felis/USB_Host_Shield_2.0/pull/614

--- usbhost.h
+++ usbhost.h
@@ -120,6 +120,8 @@ typedef SPi< P16, P18, P17, P10 > spi;
 typedef SPi< P14, P13, P12, P15 > spi;
 #elif defined(ESP32)
 typedef SPi< P18, P23, P19, P5 > spi;
+#elif defined(ARDUINO_NRF52832_FEATHER)
+typedef SPi< P14, P13, P12, P11 > spi;
 #elif defined(ARDUINO_NRF52840_FEATHER)
 typedef SPi< P26, P25, P24, P5 > spi;
 #else
